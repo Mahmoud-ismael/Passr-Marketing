a86b400d6498f9d50cbaf2ce88ca765b7bb0f7d1
HisakoLLC <hisakolimited@gmail.com>
2026-05-03 16:32:21 +0300

    fix: pin vercel adapter to 7.8.0 and use root import

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 import sitemap from '@astrojs/sitemap'
 import mdx from '@astrojs/mdx'
-import vercel from '@astrojs/vercel/edge'
+import vercel from '@astrojs/vercel'
 
 export default defineConfig({
   site: 'https://passr.eu',
