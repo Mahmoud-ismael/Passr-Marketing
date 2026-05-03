4f3295a06d7ef666d60386b9294222ad1b4665c5
HisakoLLC <hisakolimited@gmail.com>
2026-05-03 16:30:54 +0300

    fix: switch to Vercel Edge adapter to resolve Node runtime conflicts

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 import sitemap from '@astrojs/sitemap'
 import mdx from '@astrojs/mdx'
-import vercel from '@astrojs/vercel/serverless'
+import vercel from '@astrojs/vercel/edge'
 
 export default defineConfig({
   site: 'https://passr.eu',
